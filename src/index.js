3f47bf5c7ecf6c824c0205e4ebfd4f964f2575fd
Rammigas <Romanvashkevich1995@mail.ru>
2022-08-01 23:19:20 +0300

    update:solution morse

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+	let arr = []
+for (let i = 0; i < expr.length; i += 10) {
+	arr.push(expr.slice(i, i + 10))
+}
+arr = arr.map(el => el = el.replace(/^0+/, ''))
+let morseArr = arr.map(ten => ten = ten.replace(/10/g, '.'))
+morseArr = morseArr.map(eleven => eleven = eleven.replace(/11/g, '-'))
+morseArr = morseArr.map(star => star = star.replace('**********', ' '))
+for (let i = 0; i < morseArr.length; i ++) {
+	
+	if (morseArr!=='') {
+		morseArr[i] = MORSE_TABLE[morseArr[i]]
+		if (morseArr[i] === undefined){
+			morseArr[i] =' '
+		}
+	}else {
+		morseArr[i]=' '
+	}
+		
+	
+}
+return morseArr.join('')
 }
 
 module.exports = {
